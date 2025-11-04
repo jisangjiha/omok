a7c28dba5569c8d0681ff471a87bf655a086c51e
Jisang Hong <jisang1024@gmail.com>
2025-11-04 23:36:40 +0900

    fix: css properties 활용하여 tsx와 css 파일 연결

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,34 @@
+import type { CSSProperties } from "react";
 import styles from "./Board.module.css";
-import Stone from "./Stone";
 
-export default function Board() {
+interface BoardProps {
+  rowCount?: number;
+  colCount?: number;
+  rowSize?: number;
+  colSize?: number;
+}
+
+export default function Board({
+  rowCount = 15,
+  colCount = 15,
+  rowSize = 25,
+  colSize = 25,
+}: BoardProps) {
   return (
-    <div className={styles.board}>
-      {Array.from({ length: 15 }).map((_, index) => (
+    <div
+      className={styles.board}
+      style={
+        {
+          "--col-count": colCount,
+          "--row-size": `${rowSize}px`,
+          "--col-size": `${colSize}px`,
+        } as CSSProperties
+      }
+    >
+      {Array.from({ length: rowCount }).map((_, index) => (
         <div key={index} className={styles.grid}>
-          {Array.from({ length: 15 }).map((_, index) => (
+          {Array.from({ length: colCount }).map((_, index) => (
             <div key={index} className={styles.rockGrid}>
-              {/*  <Stone />*/}
               {Array.from({ length: 4 }).map((_, index) => (
                 <div key={index}></div>
               ))}

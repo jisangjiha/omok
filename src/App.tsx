13672e11492afdd8f7bb5eb45494655524f711c1
Jisang Hong <jisang1024@gmail.com>
2025-11-10 15:11:21 +0900

    fix: board 중복 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import BoardCustom from "./components/BoardCustom";
-import Board from "./components/Board";
 
 function App() {
   return (
     <>
       <BoardCustom />
-      <Board rowCount={0} colCount={0} />
     </>
   );
 }
